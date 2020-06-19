c2bdfe9d3274c220f6dbd2812d9d87284272b2b9
773899206 <773899206@qq.com>
2020-06-19 17:59:08 +0100

    1

--- DublinBus/DublinBus/settings.py
+++ DublinBus/DublinBus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f6=z$$*8-iauj0ur-ctj0e=ys&1_oq^!wkx%f!v3)z#k)_qc3&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['137.43.49.65']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DublinBus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
