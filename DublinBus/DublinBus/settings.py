d889405ee3aa4819390dbb5a644ed16609c1c27b
773899206 <773899206@qq.com>
2020-06-27 14:05:32 +0100

    database

--- DublinBus/DublinBus/settings.py
+++ DublinBus/DublinBus/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'mydb',
         'USER':'root',
         'PASSWORD': 'abc963215',
-        'HOST‘: ’127.0.0.1',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
