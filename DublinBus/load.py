7157191336145d74f4f6f0bad188ff70b7803d19
773899206 <773899206@qq.com>
2020-06-28 13:03:01 +0100

    load

--- DublinBus/load.py
+++ DublinBus/load.py
@@ -35,6 +35,8 @@ def load_stops():
         
         value = (route_id, stop_name, stop_lat, stop_lon)
         sql='insert into stops values ("%s","%s","%s","%s");'
+        print(sql)
+        
         cursor.execute(sql % value)
         i+=1
         
@@ -70,6 +72,8 @@ def load_route():
         
         value = (direction_id, route_id, stop_sequence, stop_id)
         sql='insert into route_relation values ("%s","%s","%s","%s");'
+        print(sql)
+        
         cursor.execute(sql % value)
         i+=1
     
