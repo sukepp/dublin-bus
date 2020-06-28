a106be957feddebe658cb7c50cac7438c865e72f
773899206 <773899206@qq.com>
2020-06-28 13:02:00 +0100

    load

--- DublinBus/load.py
+++ DublinBus/load.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.db import connection
 import json
 
-def load():
+def load_stops():
     cursor = connection.cursor()
     cursor.execute("DROP TABLE IF EXISTS stops")
     
@@ -22,7 +22,7 @@ def load():
     f=open(r"./static_data/stops.json", "r")
     out = f.read()
     f.close()
-    tmp = json.dumps(out)
+   
     tmp = json.loads(out)
     num = len(tmp)
     
@@ -37,5 +37,44 @@ def load():
         sql='insert into stops values ("%s","%s","%s","%s");'
         cursor.execute(sql % value)
         i+=1
-load()
+        
+def load_route():
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS route_relation")
+    
+    sql=""" CREATE TABLE IF NOT EXISTS route_relation (
+       
+        direction_id VARCHAR(256), 
+        route_id VARCHAR(256), 
+        stop_sequence VARCHAR(256),
+        stop_id VARCHAR(256)
+        )
+    
+    """
+    
+    cursor.execute(sql)
+    
+    f=open(r"./static_data/route_relation.json", "r")
+    out = f.read()
+    f.close()
+   
+    tmp = json.loads(out)
+    num = len(tmp)
+    
+    i=0
+    while i < num:
+        direction_id = tmp[i]['stop_id']
+        route_id = tmp[i]['stop_name']
+        stop_sequence = tmp[i]['stop_lat']
+        stop_id = tmp[i]['stop_lon']
+        
+        value = (direction_id, route_id, stop_sequence, stop_id)
+        sql='insert into route_relation values ("%s","%s","%s","%s");'
+        cursor.execute(sql % value)
+        i+=1
+    
+    
+    
+#load_stops()
+load_route()
     
